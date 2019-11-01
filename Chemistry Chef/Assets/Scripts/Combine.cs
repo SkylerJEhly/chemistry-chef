a5e9cd753bb0ce3f67e994d212e4db7cb5accd3e
SkylerJEhly <skyler.ehly@gmail.com>
2019-11-01 22:19:35 +0000

    Add controlers, movement, and grabbing

---
+++ Chemistry Chef/Assets/Scripts/Combine.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combine : MonoBehaviour
+{
+    //the transform is the object that is made
+    //drag the prefab into the script
+    public Transform c;
+    public GameObject a;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //place in object a
+        //both objects must have ridged bodies
+        a = GameObject.Find("Flask_01");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("detected");
+
+        //object to collide with
+        if (collision.gameObject.name == "Flask_05")
+        {
+            //Debug.Log("detected");
+            Vector3 v1 = a.transform.position;
+            Vector3 v2 = transform.position;
+            Vector3 v3 = (v1 + v2) / 2;
+            Destroy(collision.gameObject);
+            
+            Destroy(a);
+
+            Instantiate(c, v3, Quaternion.identity);
+        }
+    }
+}
